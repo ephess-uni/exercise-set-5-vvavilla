f9a4e0003a0b016491b52628021458ffe6833c35
vvavilla <123356445+vvavilla@users.noreply.github.com>
2023-03-03 18:41:45 -0600

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -5,9 +5,14 @@ This module contains an entry point that:
 - shifts and scales the data to a mean of 0 and a standard deviation 1
 - writes the file to the output file argument
 """
+import os
 import numpy as np
-from argparse import ArgumentParser
-
+import argparse
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
+    
 if __name__ == "__main__":
     # Create your argument parser object here.
     # Collect the filename arguments from the command line
@@ -16,4 +21,24 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    desc = "This program applies a standard scale transform to the data in infile and writes it to outfile."
+    root_dir = get_repository_root()
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument("infile", type=argparse.FileType('r'))
+
+    parser.add_argument("outfile", type=argparse.FileType('w'))
+
+    args = parser.parse_args()
+
+    raw_data = np.loadtxt(args.infile)
+
+    raw_data = raw_data-raw_data.mean()
+
+    standard_deviation=raw_data.std()
+
+    processed=raw_data/standard_deviation
+
+    outfile = args.outfile
+    np.savetxt(outfile, processed, fmt='%.2e')
